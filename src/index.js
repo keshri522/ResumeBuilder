3f52866373ed397fe4adf8209593d43adde2f3db
keshri522 <rkeshri522@gmail.com>
2023-04-08 21:26:05 +0530

    Added hash routers

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import store from './Components/Redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Provider store={store}>
-<BrowserRouter>
-<App />
-</BrowserRouter>
+<HashRouter>
+    <App/>
+</HashRouter>
 </Provider>
 
 
